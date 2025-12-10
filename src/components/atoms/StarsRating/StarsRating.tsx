a1ce02ba6f48e10d7f6521018f6e54f01af07c2a
Norbert Szorc <n.szorc@gmail.com>
2025-12-10 13:25:51 +0100

    Working on "Testinmonials" section

---
+++ src/components/atoms/StarsRating/StarsRating.tsx
@@ -0,0 +1,13 @@
+import { Star } from "lucide-react";
+
+export const StarsRating = () => {
+  return (
+    <div className="flex gap-1">
+      <Star fill="#FFC107" strokeWidth={0} />
+      <Star fill="#FFC107" strokeWidth={0} />
+      <Star fill="#FFC107" strokeWidth={0} />
+      <Star fill="#FFC107" strokeWidth={0} />
+      <Star fill="#FFC107" strokeWidth={0} />
+    </div>
+  );
+};
