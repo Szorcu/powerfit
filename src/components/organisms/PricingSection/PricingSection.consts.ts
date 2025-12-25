e7fe622ce3b26312581545174a5f43a1c36ec066
Norbert Szorc <n.szorc@gmail.com>
2025-12-25 22:47:36 +0100

    Finished "Pricing" section

---
+++ src/components/organisms/PricingSection/PricingSection.consts.ts
@@ -0,0 +1,23 @@
+export const PRICING_CARDS = [
+  {
+    title: "Wejście jednorazowe",
+    price: "od 45,00 zł",
+    text: "Jednorazowy dostęp do siłowni, zajęć fitness oraz rollmasaży w dniu zakupu.",
+  },
+  {
+    title: "Karnety open",
+    price: "od 89,00 zł",
+    text: "Płatny z góry karnet zapewniający nielimitowany dostęp do siłowni, zajęć fitness oraz rollmasaży.",
+  },
+  {
+    title: "Umowy open okresowe",
+    price: "od 99,00 zł",
+    text: "Umowa na określony czas z comiesięczną opłatą, zapewniająca nielimitowany dostęp do siłowni, zajęć fitness oraz rollmasaży.",
+  },
+  {
+    title: "Umowy open bezterminowe",
+    price: "od 119,00 zł",
+    text: "Umowa open na czas nieokreślony. Oferuje te same korzyści co wariant okresowy, z możliwością wypowiedzenia po 2 miesiącach.",
+    note: "*Obowiązuje miesięczny, płatny okres wypowiedzenia.",
+  },
+];
--- src/components/organisms/PricingSection/PricingSection.tsx
+++ src/components/organisms/PricingSection/PricingSection.tsx
@@ -1,28 +1,6 @@
 import { Button } from "@/components/atoms/Button";
 import { Card, CardContent, CardHeader } from "@/components/atoms/Card";
-
-const CARDS = [
-  {
-    title: "Karnety open",
-    price: "od 89,00 zł",
-    text: "Lorem ipsum dolor sit amet consectetur. Arcu lobortis eget ullamcorper at. Tincidunt .",
-  },
-  {
-    title: "Umowy open",
-    price: "od 119,00 zł",
-    text: "Lorem ipsum dolor sit amet consectetur. Arcu lobortis eget ullamcorper at. Tincidunt .",
-  },
-  {
-    title: "Wejście jednorazowe",
-    price: "od 89,00 zł",
-    text: "Lorem ipsum dolor sit amet consectetur. Arcu lobortis eget ullamcorper at. Tincidunt .",
-  },
-  {
-    title: "Pakiety",
-    price: "od 119,00 zł",
-    text: "Lorem ipsum dolor sit amet consectetur. Arcu lobortis eget ullamcorper at. Tincidunt .",
-  },
-];
+import { PRICING_CARDS } from "./PricingSection.consts";
 
 export const PricingSection = () => {
   return (
@@ -30,6 +8,7 @@ export const PricingSection = () => {
       <div className="container grid gap-8 py-7 sm:py-20 xl:grid-cols-[auto_2fr] xl:gap-12 2xl:gap-40">
         <div className="flex flex-col gap-8">
           <h2 className="max-w-80">Sprawdź nasze karnety</h2>
+
           <Button asChild className="w-fit">
             <a
               href="https://powerfit-bialystok.cms.efitness.com.pl/kup-karnet"
@@ -40,23 +19,39 @@ export const PricingSection = () => {
             </a>
           </Button>
         </div>
-        <ul className="grid gap-5 md:grid-cols-2">
-          {CARDS.map((card, index) => (
-            <li key={index}>
-              <Card className="group border-border/50 hover:bg-primary/5 hover:border-primary/50 gap-8 shadow-none transition-all duration-500">
-                <CardHeader>
-                  <h3 className="text-xl">{card.title}</h3>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-8">
-                  <p className="font-zalando group-hover:text-primary text-4xl font-semibold text-[#C3C3C3] transition-all duration-300">
-                    {card.price}
-                  </p>
-                  <p>{card.text}</p>
-                </CardContent>
-              </Card>
-            </li>
-          ))}
-        </ul>
+        <div className="flex flex-col gap-4">
+          <ul className="grid gap-5 lg:grid-cols-2">
+            {PRICING_CARDS.map((card, index) => (
+              <li key={index}>
+                <Card className="group border-border/50 hover:bg-primary/5 hover:border-primary/50 h-full gap-8 shadow-none transition-all duration-500">
+                  <CardHeader>
+                    <h3 className="text-xl">{card.title}</h3>
+                  </CardHeader>
+                  <CardContent className="flex flex-col gap-8">
+                    <p className="font-zalando group-hover:text-primary text-4xl font-semibold text-[#C3C3C3] transition-all duration-300">
+                      {card.price}
+                    </p>
+                    <div>
+                      <p>{card.text}</p>
+
+                      {card.note && (
+                        <p className="text-muted-foreground/70 mt-2 text-sm italic">
+                          {card.note}
+                        </p>
+                      )}
+                    </div>
+                  </CardContent>
+                </Card>
+              </li>
+            ))}
+          </ul>
+
+          <span className="text-muted-foreground/70 text-sm italic">
+            *Przy wszystkich karnetach i umowach, z wyłączeniem karnetów
+            tygodniowych, obowiązuje jednorazowa opłata 39 zł za kartę
+            członkowską.
+          </span>
+        </div>
       </div>
     </section>
   );
