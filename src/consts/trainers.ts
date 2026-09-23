028ce58dd62374fe6d6d8e98625e19ff028bcd3a
Norbert Szorc <n.szorc@gmail.com>
2026-09-23 19:39:44 +0200

    Updated trainers list

--- src/consts/trainers.ts
+++ src/consts/trainers.ts
@@ -1,24 +1,5 @@
 export const TRAINERS = [
   {
-    img: "trainer-krystian.png",
-    name: "Krystian Krupiński",
-    description:
-      "Trener personalny oraz trener dyżurny odpowiedzialny za pomoc ćwiczącym i nadzór nad porządkiem na siłowni; magister wychowania fizycznego, certyfikowany trener personalny, specjalizuje się w trójboju siłowym, treningach siłowych, zajęciach fitness oraz treningach obwodowych, posiada doświadczenie jako zawodnik i medalista sportów sylwetkowych oraz prowadzi treningi ukierunkowane na rozwój siły, poprawę sylwetki i redukcję tkanki tłuszczowej.",
-    classes: ["Obwodowy Power"],
-  },
-  {
-    img: "trainer-roksana.png",
-    name: "Roksana Czerwińska",
-    description:
-      "Instruktorka fitness i trener personalny z 9-letnim doświadczeniem; specjalizuje się w treningu siłowym, kształtowaniu sylwetki, treningu funkcjonalnym oraz pracy z mięśniami głębokimi.",
-    classes: [
-      "Modelowanie sylwetki",
-      "Sexy body",
-      "Siła i progres",
-      "Trening obwodowy z Roksaną",
-    ],
-  },
-  {
     img: "trainer-piotr.png",
     name: "Piotr Sidun",
     description:
@@ -26,49 +7,11 @@ export const TRAINERS = [
     classes: ["Stretching zdrowego kręgosłupa"],
   },
   {
-    img: "trainer-patrycja.png",
-    name: "Patrycja Matuszewska",
-    description:
-      "Instruktor fitness i trener personalny z wieloletnim doświadczeniem, prowadzi zajęcia fitness oraz treningi personalne ukierunkowane na poprawę kondycji i redukcję masy ciała.",
-    classes: ["Chudnij z Patrycją"],
-  },
-  {
-    img: "trainer-ela.png",
-    name: "Elżbieta Łabejsza",
-    description:
-      "Instruktorka jogi praktykująca od 2015 roku, ukończyła szkolenia m.in. u prof. Rajesh Kumar Sanyasi Bhargava oraz liczne kursy i warsztaty w Olala Yoga & Pilates School; specjalizuje się w jodze hormonalnej, yin jodze, jodze somatycznej oraz pracy z kręgosłupem i mięśniami głębokimi, prowadzi zajęcia grupowe i indywidualne.",
-    classes: [
-      "Fitball",
-      "Joga",
-      "Joga dla kręgosłupa",
-      "Joga dla początkujących",
-      "Joga Vinyasa flow",
-      "Joga slow flow",
-      "Somatic joga",
-      "Yin joga",
-      "Zdrowy kręgosłup",
-    ],
-  },
-  {
     img: "trainer-anna.png",
     name: "Anna Sulewska",
     description:
       "Instruktorka fitness oraz trenerka personalna; prowadzi zajęcia ukierunkowane na redukcję tkanki tłuszczowej i poprawę kondycji, oraz treningi personalne z naciskiem na naukę prawidłowych wzorców ruchowych.",
     classes: ["Step cardio"],
-  },
-  {
-    img: "trainer-alan.png",
-    name: "Alan Ahmad",
-    description:
-      "Trener personalny z wieloletnim doświadczeniem; specjalizuje się w treningach siłowych, eksplozywnościowych i wytrzymałościowych, prowadzi treningi ukierunkowane na kompleksowy rozwój sprawności fizycznej, z naciskiem na poprawną technikę, bezpieczeństwo oraz progres, pracując z osobami o zróżnicowanych celach i poziomie zaawansowania.",
-    classes: ["Superhero training"],
-  },
-  {
-    img: "trainer-asia.png",
-    name: "Joanna Przydacz",
-    description:
-      "Instruktorka pilatesu z 2-letnim doświadczeniem; prowadzi zajęcia pilates, łącząc elementy kontroli ruchu, stabilizacji i płynności, pracuje z grupami o zróżnicowanym poziomie zaawansowania.",
-    classes: ["Pilates"],
   },
   {
     img: "trainer-lukasz.png",
@@ -91,5 +34,71 @@ export const TRAINERS = [
       "Siła i progres",
       "Turbo spalanie",
     ],
+  },
+  {
+    img: "trainer-patrycja.png",
+    name: "Patrycja Matuszewska",
+    description:
+      "Instruktor fitness i trener personalny z wieloletnim doświadczeniem, prowadzi zajęcia fitness oraz treningi personalne ukierunkowane na poprawę kondycji i redukcję masy ciała.",
+    classes: ["Chudnij z Patrycją"],
+  },
+  {
+    img: "trainer-kamil.png",
+    name: "Kamil Gawełko",
+    description:
+      "Trener z 12-letnim doświadczeniem, specjalizujący się w treningach medycznych oraz pracy z osobami z niepełnosprawnościami. Prowadzi również intensywne zajęcia fitness o charakterze interwałowym, ukierunkowane na poprawę kondycji, wydolności i ogólnej sprawności fizycznej.",
+    classes: [
+      "Trening HIIT - grupa początkująca, Trening HIIT - grupa zaawansowana",
+    ],
+  },
+  {
+    img: "trainer-roksana.png",
+    name: "Roksana Czerwińska",
+    description:
+      "Instruktorka fitness i trener personalny z 9-letnim doświadczeniem; specjalizuje się w treningu siłowym, kształtowaniu sylwetki, treningu funkcjonalnym oraz pracy z mięśniami głębokimi.",
+    classes: [
+      "Modelowanie sylwetki",
+      "Sexy body",
+      "Siła i progres",
+      "Trening obwodowy z Roksaną",
+    ],
+  },
+  {
+    img: "trainer-asia.png",
+    name: "Joanna Przydacz",
+    description:
+      "Instruktorka pilatesu z 2-letnim doświadczeniem; prowadzi zajęcia pilates, łącząc elementy kontroli ruchu, stabilizacji i płynności, pracuje z grupami o zróżnicowanym poziomie zaawansowania.",
+    classes: ["Pilates"],
+  },
+  {
+    img: "trainer-krystian.png",
+    name: "Krystian Krupiński",
+    description:
+      "Trener personalny oraz trener dyżurny odpowiedzialny za pomoc ćwiczącym i nadzór nad porządkiem na siłowni; magister wychowania fizycznego, certyfikowany trener personalny, specjalizuje się w trójboju siłowym, treningach siłowych, zajęciach fitness oraz treningach obwodowych, posiada doświadczenie jako zawodnik i medalista sportów sylwetkowych oraz prowadzi treningi ukierunkowane na rozwój siły, poprawę sylwetki i redukcję tkanki tłuszczowej.",
+    classes: ["Obwodowy Power"],
+  },
+  {
+    img: "trainer-ela.png",
+    name: "Elżbieta Łabejsza",
+    description:
+      "Instruktorka jogi praktykująca od 2015 roku, ukończyła szkolenia m.in. u prof. Rajesh Kumar Sanyasi Bhargava oraz liczne kursy i warsztaty w Olala Yoga & Pilates School; specjalizuje się w jodze hormonalnej, yin jodze, jodze somatycznej oraz pracy z kręgosłupem i mięśniami głębokimi, prowadzi zajęcia grupowe i indywidualne.",
+    classes: [
+      "Fitball",
+      "Joga",
+      "Joga dla kręgosłupa",
+      "Joga dla początkujących",
+      "Joga Vinyasa flow",
+      "Joga slow flow",
+      "Somatic joga",
+      "Yin joga",
+      "Zdrowy kręgosłup",
+    ],
+  },
+  {
+    img: "trainer-alan.png",
+    name: "Alan Ahmad",
+    description:
+      "Trener personalny z wieloletnim doświadczeniem; specjalizuje się w treningach siłowych, eksplozywnościowych i wytrzymałościowych, prowadzi treningi ukierunkowane na kompleksowy rozwój sprawności fizycznej, z naciskiem na poprawną technikę, bezpieczeństwo oraz progres, pracując z osobami o zróżnicowanych celach i poziomie zaawansowania.",
+    classes: ["Superhero training"],
   },
 ];
